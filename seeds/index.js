8c89f62d850c0ff279e8fb7a486a55f452dd2eab
Mandell Butler <mandell.butler@outlook.com>
2021-05-24 19:10:26 -0400

    Added index.js to seeds folder

---
+++ seeds/index.js
@@ -0,0 +1,17 @@
+//CUSTOMIZE!!!!!!!!!REF14.1.17
+
+const sequelize = require('../config/connection');
+const seedGallery = require('./galleryData');
+const seedPaintings = require('./paintingData');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  await seedGallery();
+
+  await seedPaintings();
+
+  process.exit(0);
+};
+
+seedAll();
