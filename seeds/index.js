689d015d8ae44d3d6354328bf1e1b7c2ebc94f32
Mandell Butler <mandell.butler@outlook.com>
2021-05-25 17:03:27 -0400

    fixed .env file

--- seeds/index.js
+++ seeds/index.js
@@ -1,15 +1,15 @@
 //CUSTOMIZE!!!!!!!!!REF14.1.17
 
 const sequelize = require('../config/connection');
-const seedGallery = require('./galleryData');
-const seedPaintings = require('./paintingData');
+const seedUser = require('./userData');
+// const seedPaintings = require('./paintingData');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  await seedGallery();
+  await seedUser();
 
-  await seedPaintings();
+  // await seedPaintings();
 
   process.exit(0);
 };
